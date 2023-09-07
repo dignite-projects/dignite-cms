2e95dc01697df052a2f73d03114e78a1983d7d64
杜冠魁 <hello@dignite.com>
2023-09-07 11:04:19 +0900

    Create SiteDtoExtensions.cs

---
+++ src/Dignite.Cms.Public.Application.Contracts/Dignite/Cms/Public/Sites/SiteDtoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using System.Linq;
+
+namespace Dignite.Cms.Public.Sites
+{
+    public static class SiteDtoExtensions
+    {
+        public static string GetDefaultLanguage([NotNull] this SiteDto source)
+        {
+            return source.Languages.OrderByDescending(l => l.IsDefault).First().Language;
+        }
+    }
+}

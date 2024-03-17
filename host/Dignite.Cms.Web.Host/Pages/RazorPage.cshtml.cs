09f54ed9ae3f9b575cd8d46b2b63da9205d97011
杜冠魁 <hello@dignite.com>
2024-03-17 22:46:18 +0900

    Add multi-tenant localization testing

---
+++ host/Dignite.Cms.Web.Host/Pages/RazorPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dignite.Cms.Pages
+{
+    public class RazorPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

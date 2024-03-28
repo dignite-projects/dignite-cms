37c3bc61014c3d772d2ae01fb778521b4d232c5a
2416426467 <2416426467@qq.com>
2024-03-28 20:22:35 +0800

    Delete the angular package of the cms and migrate to dignite-cms-commercial

--- angular/projects/cms/config/src/enums/route-names.ts
+++
@@ -1,3 +0,0 @@
-export const enum eCmsRouteNames {
-  Cms = 'Cms',
-}
--- angular/projects/cms/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of cms
- */
-
-export * from './lib/components/cms.component';
-export * from './lib/services/cms.service';
-export * from './lib/cms.module';
